f5e27476dd975684a598d85928dd4f9903e00576
alexsilvadedeus <116230432+AlexSilvaDeus@users.noreply.github.com>
2022-10-25 01:40:11 +0000

    finalizamos a primeira aula,exibindo as variaveis nome e idade

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 #Meu primeiro projeto python!!!
 #
 #print() = comando de saida
-print("Alo mundo do Alex!")
+print("Alo mundo!")
+
+#Quando quiser guarda uma string!(frase)
+nome = "Alex Silva de Deus"
+
+#Quando quiser guarda um numero inteiro
+idade = 31
+
+#Exibir meu nome (que esta dentro da variavel nome)
+print("Meu Nome é:"+nome)
+
+#3 opções de Exibir minha idade, é preciso colocar um STR pra converter para string ou format pra idade
+print("Minha Idade é: "+str(idade)+ " anos")
+print(f"Minha Idade é: {idade}" " anos")
+print("Minha Idade é: {} anos".for mat(idade))
+
+#Exibir nome e idade na mesma variavel
+print("Meu Nome é {} e minha Idade é {}".format(nome,idade))
+
+
 '''
 comentario em bloco
 
